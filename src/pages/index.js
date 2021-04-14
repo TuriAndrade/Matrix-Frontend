2c7d832a672fd76948e0e6932bb0f4fcc7cca3d3
Turi Vasconcelos <turivasconcelos@gmail.com>
2021-04-14 18:24:29 -0300

    Home page main section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,31 @@
+import GirlStudyingMobile from "../svgs/background/girlStudyingMobile";
+import GirlStudying from "../svgs/background/girlStudying";
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <div className="homePage__main">
+        <GirlStudying className="homePage__mainBackground homePage__mainBackground--default" />
+        <GirlStudyingMobile className="homePage__mainBackground homePage__mainBackground--tabPort" />
+        <div className="homePage__mainHeading">
+          <h1 className="h1 h1--black">Estudo ao seu alcance</h1>
+          <p className="p p--black">
+            O projeto Matrix é uma organização sem fins lucrativos que visa
+            oferecer material didático de forma gratuita e acessível a todos.
+          </p>
+          <Link href="/register">
+            <a className="btnPrimary">Comece agora</a>
+          </Link>
+        </div>
+      </div>
+      <div className="homePage__benefits">
+        <img
+          src="/bgPattern.webp"
+          alt="Background"
+          className="homePage__benefitsBackground"
+        />
+      </div>
+    </>
+  );
 }
